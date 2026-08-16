b1287385926564445d23f673465c7e94139ee152
Ravi Maurya <93729578+Ravi2977@users.noreply.github.com>
2026-08-16 09:58:20 +0530

    blog view page updated

--- blog-platform/src/app/blog/[slug]/page.js
+++ blog-platform/src/app/blog/[slug]/page.js
@@ -281,7 +281,7 @@ export default function ReadBlogPage({ params }) {
 
       {/* Blog */}
 
-      <article className="mx-auto max-w-4xl px-5 py-10 lg:px-8">
+      <article className="mx-auto max-w-7xl px-5 py-10 lg:px-8">
 
         {/* Cover */}
 
